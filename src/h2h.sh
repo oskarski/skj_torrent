e488112eb551b28905019163b2ec7e6111652fbc
Oskar Kupski <oskar.kupski@gmail.com>
2021-01-08 16:37:06 +0100

    Adjusted shel scripts for project requirments

--- src/h2h.sh
+++ src/h2h.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-HOST_A_PORT=12346
+HOST_A_PORT=10006
 HOST_A_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_a
 
-HOST_B_PORT=12347
+HOST_B_PORT=10007
 HOST_B_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_b
 
 echo "Killing applications running on port: ${HOST_A_PORT}, ${HOST_B_PORT}"
@@ -19,10 +19,10 @@ echo ""
 
 cd out || exit
 
-echo "Running Host A program"
+echo "Running Host A program on port ${HOST_A_PORT}"
 java HostMain ${HOST_A_PORT} ${HOST_A_DIR} 127.0.0.1:${HOST_B_PORT} false true &
 
-echo "Running Host B program"
+echo "Running Host B program on port ${HOST_B_PORT}"
 java HostMain ${HOST_B_PORT} ${HOST_B_DIR} 127.0.0.1:${HOST_A_PORT} true true
 
 echo "Success!"
--- src/mh.sh
+++ src/mh.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-TRACKER_PORT=12345
+TRACKER_PORT=10001
 TRACKER_ADDRESS=127.0.0.1:${TRACKER_PORT}
 
-HOST_A_PORT=12346
+HOST_A_PORT=10002
 HOST_A_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_a
 
-HOST_B_PORT=12347
+HOST_B_PORT=10003
 HOST_B_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_b
 
-HOST_C_PORT=12348
+HOST_C_PORT=10004
 HOST_C_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_c
 
+HOST_D_PORT=10005
+HOST_D_DIR=/Users/oskarkupski/IdeaProjects/SKJ_project/src/workspace/host_d
 
-echo "Killing applications running on port: ${TRACKER_PORT}, ${HOST_A_PORT}, ${HOST_B_PORT}, ${HOST_C_PORT}"
+
+echo "Killing applications running on port: ${TRACKER_PORT}, ${HOST_A_PORT}, ${HOST_B_PORT}, ${HOST_C_PORT}, ${HOST_D_PORT}"
 kill $(lsof -t -i:$TRACKER_PORT)
 kill $(lsof -t -i:$HOST_A_PORT)
 kill $(lsof -t -i:$HOST_B_PORT)
 kill $(lsof -t -i:$HOST_C_PORT)
+kill $(lsof -t -i:$HOST_D_PORT)
 
 echo ""
 
@@ -31,17 +35,20 @@ echo ""
 
 cd out || exit
 
-echo "Running HostTracker program"
+echo "Running HostTracker program on port ${TRACKER_PORT}"
 java HostTrackerMain ${TRACKER_PORT} &
 
-echo "Running Host A program"
+echo "Running Host A program on port ${HOST_A_PORT}"
 java HostMain ${HOST_A_PORT} ${HOST_A_DIR} ${TRACKER_ADDRESS} false &
 
-echo "Running Host B program"
+echo "Running Host B program on port ${HOST_B_PORT}"
 java HostMain ${HOST_B_PORT} ${HOST_B_DIR} ${TRACKER_ADDRESS} false &
 
-echo "Running Host C program"
-java HostMain ${HOST_C_PORT} ${HOST_C_DIR} ${TRACKER_ADDRESS} true
+echo "Running Host C program on port ${HOST_C_PORT}"
+java HostMain ${HOST_C_PORT} ${HOST_C_DIR} ${TRACKER_ADDRESS} false &
+
+echo "Running Host D program on port ${HOST_D_PORT}"
+java HostMain ${HOST_D_PORT} ${HOST_D_DIR} ${TRACKER_ADDRESS} true
 
 echo "Success!"
 
